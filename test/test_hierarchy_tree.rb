ca166291cdbbcba18b8657d87026ddf314f44427
Victor <victorcorcos@gmail.com>
2020-02-28 12:22:50 -0300

    Working single test

--- test/test_hierarchy_tree.rb
+++ test/test_hierarchy_tree.rb
@@ -4,47 +4,25 @@ require 'active_record'
 require 'active_support/core_ext/object/inclusion.rb'
 
 class TestHierarchyTree < Minitest::Test
-  # when the Book belongs to a Person
-  class Book < ActiveRecord::Base
-    belongs_to :person
-  end
-
-  class Person < ActiveRecord::Base
-  end
+  Object.const_set('Book', Class.new(ActiveRecord::Base))
+  Object.const_set('Page', Class.new(ActiveRecord::Base))
+  Object.const_set('Line', Class.new(ActiveRecord::Base))
+  Object.const_set('Word', Class.new(ActiveRecord::Base))
+  Object.const_set('Letter', Class.new(ActiveRecord::Base))
+  Book.class_eval { has_many :pages }
+  Page.class_eval { has_many :lines }
+  Line.class_eval { has_many :words }
+  Word.class_eval { has_many :letters }
 
   def test_should_not_return_the_person
-    assert_equal(Hierarchy.associations(Book), [])
+    assert_equal(Hierarchy.associations(Book), [{ pages: [{ lines: [{ words: [:letters] }] }] }])
   end
 
   def test_should_not_return_the_descendants
-    assert_equal(Hierarchy.descendants(Book), [])
+    assert_equal(Hierarchy.descendants(Book), %w[Page Line Word Letter])
   end
 
   def test_should_not_be_a_loop
     assert_equal(Hierarchy.loop?(Book), false)
-  end
-
-  # when a Person have Words through Books
-  class Word < ActiveRecord::Base
-  end
-
-  class Person < ActiveRecord::Base
-    has_many :words, through: :book, autosave: false
-  end
-
-  class Book < ActiveRecord::Base
-    has_many :words
-  end
-
-  def test_should_not_return_the_through_association
-    assert_equal(Hierarchy.associations(Person), [])
-  end
-
-  def test_should_not_return_the_descendants_2
-    assert_equal(Hierarchy.descendants(Person), [])
-  end
-
-  def test_should_not_be_a_loop_2
-    assert_equal(Hierarchy.loop?(Person), false)
   end
 end
